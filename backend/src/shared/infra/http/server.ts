94f89c8a10d8f9d522f4acb0b25730e8bbe21906
wvcneto <wvcneto@gmail.com>
2020-07-20 02:17:31 -0300

    angular-dashboard-template

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import '@shared/infra/typeorm';
@@ -6,6 +7,8 @@ import routes from '@shared/infra/http/routes';
 import AppError from '@shared/errors/AppError';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
